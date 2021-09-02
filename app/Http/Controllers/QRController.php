59cfbd0a63d33909f612f937902da64caa4c5b25
dinushchathurya <dinushchathurya21@gmail.com>
2021-09-02 09:30:51 +0530

    :sparkles: Create function to generate QR Code

--- app/Http/Controllers/QRController.php
+++ app/Http/Controllers/QRController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SimpleSoftwareIO\QrCode\Facades\QrCode;
 
 class QRController extends Controller
 {
     public function index()
     {
         return view('welcome');
+    }
+
+    public function create()
+    {
+        QrCode::generate('https://codingtricks.io', '../public/QRCode.svg');
+	    return redirect()->route('welcome');
     }
 }
